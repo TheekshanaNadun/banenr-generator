da3d5c4b6d2be72760c17597ab6b8dff94eab86f
Kaikawala Gedara Theekshana Nadun Bandara Kaikawla <74358481+TheekshanaNadun@users.noreply.github.com>
2025-08-02 19:44:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.post('/generate', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: 'new',
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
